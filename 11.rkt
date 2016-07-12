ac2ef4465f070f33c473a5078f02ca667a6eddea
kminchul <k.minchul95@gmail.com>
2016-07-12 15:01:11 +0900

    P11. Modified run-length encoding.

---
+++ 11.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(define (encode-modified myList)
+  (define (my-len myList)
+    (if (null? myList)
+        0
+        (+ 1 (my-len (rest myList)))))
+  (define (pack myList)
+    (define (inner-pack myList packing)
+      (if (null? myList)
+          (list packing)
+          (if (null? packing)
+              (inner-pack (rest myList) (list (first myList)))
+              (if (equal? (first myList) (first packing))
+                  (inner-pack (rest myList) (cons (first myList) packing))
+                  (cons packing (inner-pack myList `()))))))
+    (inner-pack myList `()))
+  (define (inner-encode packList)
+    (if (null? packList)
+        null
+        (if (= 1 (my-len (first packList)))
+            (cons (first (first packList)) (inner-encode (rest packList)))
+            (cons (list (my-len (first packList)) (first (first packList))) (inner-encode (rest packList))))))
+  (inner-encode (pack myList)))
