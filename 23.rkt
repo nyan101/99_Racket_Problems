842c6e71c1ad12c96a50fd37169995fd663454d2
kminchul <k.minchul95@gmail.com>
2016-07-13 11:23:20 +0900

    P23. Extract a given number of randomly selected elements from a list.

---
+++ 23.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(define (rnd-select myList n)
+  (define (element-at myList idx)
+  (if (null? myList)
+      null
+      (if (= idx 1)
+          (first myList)
+          (element-at (rest myList) (- idx 1)))))
+  (define (remove-at myList idx)
+    (if (null? myList)
+        null
+        (if (= idx 1)
+            (rest myList)
+            (cons (first myList) (remove-at (rest myList) (- idx 1))))))
+  (if (or (null? myList) (<= n 0))
+      null
+      (let ([randIdx (+ (random (length myList)) 1)])
+        (cons (element-at myList randIdx) (rnd-select (remove-at myList randIdx) (- n 1))))))
